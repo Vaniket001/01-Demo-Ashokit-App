fa1258323a7cb225a9da7125d3c9196d39c0e9df
Aniket <kumaraniket900@gmail.com>
2022-04-14 06:07:58 +0530

    added sop statement in Demo.java and Test.java

---
+++ Demo.java
@@ -0,0 +1,5 @@
+public class Demo1{
+    public void md1(){
+        System.out.println();
+    }
+}
---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test1{
+
+    public void m1(){
+        System.out.println(){
+            
+        }
+    }
+}
